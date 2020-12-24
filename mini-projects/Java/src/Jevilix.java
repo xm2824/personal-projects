d5f48bc2566f3043100dc08d1bb255c9ca5a81a2
Tao Xiang <ge25pof@mytum.de>
2020-12-24 18:36:15 +0100

    1

---
+++ mini-projects/Java/src/Jevilix.java
@@ -0,0 +1,10 @@
+
+public class Jevilix {
+    public static void main(String[] args) {
+        System.out.println("hello wolrd");
+        Integer a = 20;
+        System.out.println(a.byteValue());
+        double ab =Math.ceil(11.2);
+        System.out.println(ab);
+    }
+}
